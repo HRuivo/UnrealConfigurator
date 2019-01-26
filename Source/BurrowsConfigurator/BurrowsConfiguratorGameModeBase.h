8086f3c7236f87296aafdc9c2433baf47160f04d
Hugo Ruivo <hruivo89@gmail.com>
2019-01-26 12:33:47 +0100

    Added ue4 project files

---
+++ Source/BurrowsConfigurator/BurrowsConfiguratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BurrowsConfiguratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BURROWSCONFIGURATOR_API ABurrowsConfiguratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
